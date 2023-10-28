9122433429aaa6ec081a74a4e42aea32a102b79c
desimingo <desimingo15@gmail.com>
2023-10-28 16:48:02 -0300

    agrego cambio en base de datos

--- app/src/main/java/com/example/connectsalud/AdminSQLiteOpenHelper.java
+++ app/src/main/java/com/example/connectsalud/AdminSQLiteOpenHelper.java
@@ -7,13 +7,24 @@ import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.Nullable;
 
 public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
+    // crear constructor:
     public AdminSQLiteOpenHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
 
+    // crear tablas de la DB:
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        db.execSQL ("create table usuarios (\")
+        sqLiteDatabase.execSQL("create table usuarios (\n" +
+                "\tdni integer primary key,\n" +
+                "\tnombre text,\n" +
+                "\tapellido text,\n" +
+                "\ttelefono integer,\n" +
+                "\tnacimiento text,\n" +
+                "\temail text,\n" +
+                "\tpass text,\n" +
+                "\tpassagain text\n" +
+                ")");
     }
 
     @Override
