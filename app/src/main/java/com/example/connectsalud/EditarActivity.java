aa0559e220067bad56c33b418ed90bd0fa5bf76a
samira agostina sanchez escudero <samira.sanchez99@outlook.com>
2023-10-27 23:41:30 -0300

    se agregan activitys para CRUD

---
+++ app/src/main/java/com/example/connectsalud/EditarActivity.java
@@ -0,0 +1,86 @@
+package com.example.connectsalud;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.connectsalud.db.DbUsuarios;
+import com.example.connectsalud.entidades.Usuario;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class EditarActivity extends AppCompatActivity {
+
+    EditText txtNombre, txtTelefono, txtCorreo;
+    Button btnGuarda;
+    FloatingActionButton fabEditar, fabEliminar;
+    boolean correcto = false;
+    Usuario usuario;
+    int id = 0;
+
+    @SuppressLint("RestrictedApi")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ver);
+
+        txtNombre = findViewById(R.id.txtNombre);
+        txtTelefono = findViewById(R.id.txtTelefono);
+        txtCorreo = findViewById(R.id.txtCorreoElectronico);
+        btnGuarda = findViewById(R.id.btnGuarda);
+        fabEditar = findViewById(R.id.fabEditar);
+        fabEditar.setVisibility(View.INVISIBLE);
+        fabEliminar = findViewById(R.id.fabEliminar);
+        fabEliminar.setVisibility(View.INVISIBLE);
+
+        if (savedInstanceState == null) {
+            Bundle extras = getIntent().getExtras();
+            if (extras == null) {
+                id = Integer.parseInt(null);
+            } else {
+                id = extras.getInt("ID");
+            }
+        } else {
+            id = (int) savedInstanceState.getSerializable("ID");
+        }
+
+        final DbUsuarios dbUsuarios = new DbUsuarios(EditarActivity.this);
+        usuario = dbUsuarios.verUsuario(id);
+
+        if (usuario != null) {
+            txtNombre.setText(usuario.getNombre());
+            txtTelefono.setText(usuario.getTelefono());
+            txtCorreo.setText(usuario.getCorreo_electornico());
+        }
+
+        btnGuarda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!txtNombre.getText().toString().equals("") && !txtTelefono.getText().toString().equals("")) {
+                    correcto = dbUsuarios.editarUsuario(id, txtNombre.getText().toString(), txtTelefono.getText().toString(), txtCorreo.getText().toString());
+
+                    if(correcto){
+                        Toast.makeText(EditarActivity.this, "REGISTRO MODIFICADO", Toast.LENGTH_LONG).show();
+                        verRegistro();
+                    } else {
+                        Toast.makeText(EditarActivity.this, "ERROR AL MODIFICAR REGISTRO", Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    Toast.makeText(EditarActivity.this, "DEBE LLENAR LOS CAMPOS OBLIGATORIOS", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    private void verRegistro(){
+        Intent intent = new Intent(this, VerActivity.class);
+        intent.putExtra("ID", id);
+        startActivity(intent);
+    }
+}
