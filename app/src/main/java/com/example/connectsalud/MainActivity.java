d8c646737445e0aafa74e936ae2b6b3173807b86
desimingo <desimingo15@gmail.com>
2023-10-28 01:17:03 -0300

    agrego cambios

--- app/src/main/java/com/example/connectsalud/MainActivity.java
+++ app/src/main/java/com/example/connectsalud/MainActivity.java
@@ -1,51 +1,14 @@
 package com.example.connectsalud;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.database.sqlite.SQLiteDatabase;
-import com.example.connectsalud.Adaptadores;
-import com.example.connectsalud.Adaptadores.ListaUsuariosAdapter;
-import com.example.connectsalud.db.DbUsuarios;
-import com.example.connectsalud.db.DbHelper;
-import com.example.connectsalud.Entidades.Usuarios;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    RecyclerView listaUsuarios;
-    ArrayList<Usuarios> listaArrayUsuarios;
-    ListaUsuariosAdapter adapter;
+
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        txtBuscar = findViewById(R.id.txtBuscar);
-        listaUsuarios = findViewById(R.id.listaUsuarios);
-        fabNuevo = findViewById(R.id.favNuevo);
-        listaUsuarios.setLayoutManager(new LinearLayoutManager(this));
-
-        DbUsuarios dbUsuarios = new DbUsuarios(MainActivity.this);
-
-        listaArrayUsuarios = new ArrayList<>();
-
-        adapter = new ListaUsuariosAdapter(dbUsuarios.mostrarUsuarios());
-        listaUsuarios.setAdapter(adapter);
-
-        fabNuevo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                nuevoRegistro();
-            }
-        });
-
-        txtBuscar.setOnQueryTextListener(this);
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,4 +25,5 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, RegistroPaciente.class);
         startActivity(intent);
     }
+
 }
