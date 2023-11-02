6a61df727242f7bed779775ceb77085f25be965e
desimingo <desimingo15@gmail.com>
2023-11-02 02:21:44 -0300

    creando base de datos reserva

--- app/src/main/java/com/example/connectsalud/AdminSQLiteOpenHelper.java
+++ app/src/main/java/com/example/connectsalud/AdminSQLiteOpenHelper.java
@@ -1,34 +1,60 @@
 package com.example.connectsalud;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-
-import androidx.annotation.Nullable;
+import android.content.ContentValues;
 
 public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
-    // crear constructor:
-    public AdminSQLiteOpenHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    private static final String DATABASE_NAME = "db1";
+    private static final int DATABASE_VERSION = 1;
+
+
+    public AdminSQLiteOpenHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
-    // crear tablas de la DB:
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL("create table usuarios (\n" +
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table usuarios (\n" +
                 "\tdni integer primary key,\n" +
                 "\tnombre text,\n" +
                 "\tapellido text,\n" +
                 "\ttelefono integer,\n" +
-                "\tnacimiento text,\n" +
+                "\tnacimient" +
+                "" +
+                "o text,\n" +
                 "\temail text,\n" +
                 "\tpass text,\n" +
-                "\ttipousuario text\n" +
+                "\tpassagain text\n" +
                 ")");
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Aquí puedes implementar la lógica de actualización si es necesario.
+    }
 
+    public Cursor leerPacientes() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String[] projection = {"dni", "nombre", "apellido", "telefono", "nacimiento", "email"};
+        return db.query("usuarios", projection, null, null, null, null, null);
+    }
+
+    public long insertarUsuario(String dni, String nombre, String apellido, String telefono, String nacimiento, String email, String pass, String passagain) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("dni", dni);
+        values.put("nombre", nombre);
+        values.put("apellido", apellido);
+        values.put("telefono", telefono);
+        values.put("nacimiento", nacimiento);
+        values.put("email", email);
+        values.put("pass", pass);
+        values.put("passagain", passagain);
+        long newRowId = db.insert("usuarios", null, values);
+        db.close();
+        return newRowId;
     }
 }
--- app/src/main/java/com/example/connectsalud/Profile.java
+++ app/src/main/java/com/example/connectsalud/Profile.java
@@ -8,16 +8,11 @@ import android.view.View;
 
 public class Profile extends AppCompatActivity {
 
-    AdminSQLiteOpenHelper admin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
     }
 
-    public void consultarPaciente(View view) {
-        SQLiteDatabase db =admin.getWritableDatabase();
-
-    }
 
 }
